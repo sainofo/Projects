4a8f5e3ecd05931673b5bd88d86cd671ee060a2a
Nofo <96763105+sainofo@users.noreply.github.com>
2023-11-12 19:25:53 -0800

    EmailSlicers.py

---
+++ Python/EmailSlicers.py
@@ -0,0 +1,17 @@
+## Sainofo Fanene
+## Date: 11.12.23
+## Personal Project: Email Slicers
+
+print("Hello! Welcome!") ##intro
+
+## asking for your email
+print("Enter your email: ")
+email = input("Enter you email: ").strip()
+
+## getting username and domain by string slicing
+username = email[:email.index('@')]
+domain = email[email.index('@')+ 1:]
+
+## Displays the username and domain
+print(f"Username: {username}")
+print(f"Domain: {domain}")
